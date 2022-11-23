f6f3540c5061263adfbaef715ea1c27a464c2710
Ian Coto <iancotosoto@gmail.com>
2022-11-22 23:07:48 -0600

    Crea reporteProductos()
    Y lo que va entorno a ella, falta probar

--- reportesCasillerosProductos.py
+++ reportesCasillerosProductos.py
@@ -5,6 +5,8 @@
 
 # Importar librerías
 from claseUsuario import *
+from claseCompra import *
+from conversionWebService import *
 
 # Función archivos HTML
 def crearArchivoHtml(pNombre, pInfo):
@@ -42,7 +44,49 @@ def reporteCasilleros(pUsuarios):
     return crearArchivoHtml("Reporte casilleros AeroTEC", strTabla)
 
 # Productos por casillero
+def obtenerDetCasillero(pCasillero, pCompras):
+    for compras in pCompras:
+        if pCasillero == compras.obtenerNumCuenta():
+            return compras.obtenerDetalle()
+    return []
 
+def obtenerProdCasillero(pCasillero, pCompras, pProductos):
+    prodsCasillero = obtenerProdCasillero(pCasillero, pCompras)
+    productos = []
+    valorColon = cambioUSDtoCRC()
+    for compra in prodsCasillero:
+        for codigo, datos in pProductos.items():
+            if compra[0] == codigo:
+                productos.append((datos[0], datos[1][0], datos[1][0]*valorColon))
+                break
+    return productos
+
+def reportesProductos(pCasillero, pCompras, pProductos):
+    productos = obtenerProdCasillero(pCasillero, pCompras, pProductos)
+    strTabla = "<html>\n<head>\n<title> \nProductos casillero "+str(pCasillero)+" \n\
+                </title>\n</head><body><h1>Casillero "+str(pCasillero)+"</h1> \
+                <table><tr><th>Nombre del producto</th><th>Precio dólares</th> \
+                    <th>País</th><th>Precio colones</th></tr>"
+    for producto in productos:
+        strElementos = ("<tr><td>"+producto[0]+"</td><td>"+
+            producto[1]+"</td><td>"+producto[2]+"td></tr>")
+        strTabla += strElementos
+    strTabla += "</table></html>"
+    return crearArchivoHtml("Reporte productos casillero " + str(pCasillero), strTabla)
+        
+def reportesProductosAux(pCasillero, pCompras, pProductos):
+    """
+    Funcionalidad: valida los datos de entrada
+    Entradas: 
+    Salidas: resultado de crearUsuarios(pCant, pUsuarios) (list)
+    """
+    if not esEntero(pCasillero):
+        return messagebox.showerror("Formato casillero incorrecto", "Debe ingresar un número entero positivo\
+            de acuerdo a la cantidad de casilleros disponibles.")
+    elif 0 >= int(pCasillero) or int(pCasillero) > len(pCompras):
+        return messagebox.showerror("Casillero no encontrado", "Debe ingresar un número entero positivo\
+            de acuerdo a la cantidad de casilleros disponibles.")
+    return reportesProductos(int(pCasillero), pCompras, pProductos)
 
 # Pruebas
 """
