f0d53a0b2de1889392b9b776538545549e885866
Ian Coto <iancotosoto@gmail.com>
2022-11-22 21:32:00 -0600

    reportesCasilleros() creada
    Se crea la función y se prueba con datos que cambian al ejecutar.

--- reportesCasillerosProductos.py
+++ reportesCasillerosProductos.py
@@ -4,6 +4,7 @@
 # Versión: 3.10.8
 
 # Importar librerías
+from claseUsuario import *
 
 # Función archivos HTML
 def crearArchivoHtml(pNombre, pInfo):
@@ -20,7 +21,31 @@ def crearArchivoHtml(pNombre, pInfo):
 
 # Funciones reportes
 # Casilleros
+def reporteCasilleros(pUsuarios):
+    """
+    Funcionalidad: crea el archivo HTML con la información de los usuarios (sus casilleros)
+    Entradas: pUsuarios
+    Salidas: resultado crearArchivoHtml("", strTabla)
+    """
+    strTabla = "<html>\n<head>\n<title> \nCasilleros AeroTEC \n\
+                </title>\n</head><body><h1>Casilleros AeroTEC</h1> \
+                <table><tr><th>Número de casillero</th><th>Nombre completo</th> \
+                    <th>País</th><th>Método de pago</th></tr>"
+    for usuario in pUsuarios:
+        nombreTupla = usuario.obtenerNomDuenno()
+        strElementos = ("<tr><td>"+str(usuario.obtenerNumCuenta())+"</td><td>"+
+            nombreTupla[0]+" "+nombreTupla[1]+" "+nombreTupla[2]+"</td> \
+                <td>"+retornarPaisStr(usuario.obtenerPais())+"</td>\
+                    <td>"+retornarMetPagoStr(usuario.obtenerMetPago())+"</td></tr>")
+        strTabla += strElementos
+    strTabla += "</table></html>"
+    return crearArchivoHtml("Reporte casilleros AeroTEC", strTabla)
 
+usuarios = crearUsuariosAux(10, [])
 
+for i in usuarios:
+    print(i.obtenerInfo())
+
+reporteCasilleros(usuarios)
 # Productos
 
